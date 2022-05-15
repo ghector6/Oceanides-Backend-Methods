59126761cb98aa3c1f413dea31f5a5a7d3cd29d9
ghector6 <ghectormus@gmail.com>
2022-05-15 13:21:25 -0500

    Agregando eslint para syntaxis

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+        indent: ["error", 4],
+        "linebreak-style": ["error", "unix"],
+        quotes: ["error", "double"],
+        semi: ["error", "always"]
+    }
+};
