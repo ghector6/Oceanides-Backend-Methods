9354e276b91ea746b635c28f247b084f247eaf92
ghector6 <ghectormus@gmail.com>
2022-05-15 12:02:11 -0500

    Agregando prueba para ver si se lee el json

---
+++ test/utils/Reader.test.js
@@ -0,0 +1,8 @@
+const Reader = require("./../../lib/utils/Reader");
+
+describe("Test to see if reader is working", () => {
+    test("if reader is reading duh", () => {
+        const plastics = Reader.readJsonFile("./plastics.json");
+        expect(plastics.length).toBe(7);
+    })
+})
